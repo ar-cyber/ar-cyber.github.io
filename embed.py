cecf23c20f7bfc79306026f5582a0709b8a50b72
Andrew R <25roband@cbc.sa.edu.au>
2024-11-28 09:08:15 +1030

    Create embed.py

---
+++ embed.py
@@ -0,0 +1,6 @@
+# To use, run 'curl -ssL https://robinand.sockycat.net/embed.py | python3'
+
+import os
+while True:
+  os.system("firefox --kiosk https://christianbrotherscollege.softlinkhosting.com.au/oliver/junior.do")
+"
